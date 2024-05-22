fd3b6578aa2840a8dcb637c0672dcd75c3133ff0
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-05-22 19:03:43 +0200

    1

---
+++ app/Console/Commands/GenerateModels.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GenerateModels extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-models';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
