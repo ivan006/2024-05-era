1bd002f1ea29aff516c85fdd7a0d3d25eb199fcf
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-05-25 15:54:53 +0200

    1

--- app/Console/Commands/GenerateVuexOrmModels.php
+++ app/Console/Commands/GenerateVuexOrmModels.php
@@ -25,21 +25,41 @@ class GenerateVuexOrmModels extends Command
             $columns = DB::select("SHOW COLUMNS FROM $tableName");
 
             $fields = [];
-            foreach ($columns as $column) {
-                $fields[] = "'{$column->Field}': this.attr('')";
-            }
-
-            $fieldsString = implode(",\n            ", $fields);
+            $fieldsMetadata = [];
             $relations = $this->getModelRelations($tableName);
             $imports = $this->generateImports($modelName, $relations['imports']);
 
+            foreach ($columns as $column) {
+                $fieldName = $column->Field;
+                $fieldMeta = $this->generateFieldMetadata($fieldName);
+                $fields[] = "'$fieldName': this.attr('', $fieldMeta)";
+                $fieldsMetadata[] = "$fieldName: { usageType: '', linkable: true }"; // Example metadata
+            }
+
+            $fieldsString = implode(",\n            ", $fields);
+            $fieldsMetadataString = implode(",\n            ", $fieldsMetadata);
             $relationsString = implode(",\n            ", $relations['relations']);
 
             $jsModel = <<<EOT
 $imports
 
-export default class $modelName extends Model {
+export default class $modelName extends MyBaseModel {
     static entity = '$jsModelName';
+    static entityUrl = '/rest/v1/$tableName';
+
+    static parentWithables = [
+        // Add parent withables based on relations
+    ];
+
+    static rules = {
+        readables: (user) => true,
+        readable: (user, item) => true,
+        editable: (user, item) => true,
+    };
+
+    static fieldsMetadata = {
+        $fieldsMetadataString
+    };
 
     static fields() {
         return {
@@ -47,6 +67,61 @@ export default class $modelName extends Model {
             $fieldsString,
             $relationsString
         };
+    }
+
+    static displayMapSummary = {
+        // Define displayMapSummary
+    };
+
+    static displayMapFull = {
+        // Define displayMapFull
+    };
+
+    static FetchAll(relationships = [], flags = {}, moreHeaders = {}, options = { page: 1, limit: 15, filters: {}, clearPrimaryModelOnly: false }) {
+        return this.customSupabaseApiFetchAll(
+            `\${this.baseUrl}\${this.entityUrl}`,
+            [...this.parentWithables, ...relationships],
+            flags,
+            this.mergeHeaders(moreHeaders),
+            options
+        );
+    }
+
+    static FetchById(id, relationships = [], flags = {}, moreHeaders = {}) {
+        return this.customSupabaseApiFetchById(
+            `\${this.baseUrl}\${this.entityUrl}?id=eq.\${id}`,
+            id,
+            [...this.parentWithables, ...relationships],
+            flags,
+            this.mergeHeaders(moreHeaders)
+        );
+    }
+
+    static Store(entity, relationships = [], flags = {}, moreHeaders = {}) {
+        return this.customSupabaseApiStore(
+            `\${this.baseUrl}\${this.entityUrl}`,
+            entity,
+            [...this.parentWithables, ...relationships],
+            flags,
+            this.mergeHeaders(moreHeaders)
+        );
+    }
+
+    static Update(entity, relationships = [], flags = {}, moreHeaders = {}) {
+        return this.customSupabaseApiUpdate(
+            `\${this.baseUrl}\${this.entityUrl}?id=eq.\${entity.id}`,
+            entity,
+            [...this.parentWithables, ...relationships],
+            flags,
+            this.mergeHeaders(moreHeaders)
+        );
+    }
+
+    static Delete(entityId, options = { flags: {}, moreHeaders: {} }) {
+        return this.customSupabaseApiDelete(
+            `\${this.baseUrl}\${this.entityUrl}?id=eq.\${entityId}`,
+            entityId
+        );
     }
 }
 EOT;
@@ -84,19 +159,10 @@ EOT;
         return ['relations' => $relations, 'imports' => $imports];
     }
 
-    protected function extractRelationDetails($relationString)
+    protected function generateFieldMetadata($fieldName)
     {
-        preg_match('/\'App\\\\Models\\\\(\w+)\'/', $relationString, $modelMatch);
-        preg_match('/,\s*\'(\w+)\'/', $relationString, $foreignKeyMatch);
-        preg_match('/relatedPivotKey:\s*\'(\w+)\'/', $relationString, $relatedForeignKeyMatch);
-        preg_match('/pivotTable:\s*\'(\w+)\'/', $relationString, $pivotTableMatch);
-
-        return [
-            'relatedModel' => $modelMatch ? $modelMatch[1] : null,
-            'foreignKey' => $foreignKeyMatch ? $foreignKeyMatch[1] : null,
-            'relatedForeignKey' => $relatedForeignKeyMatch ? $relatedForeignKeyMatch[1] : null,
-            'pivotTable' => $pivotTableMatch ? $pivotTableMatch[1] : null
-        ];
+        // Generate field metadata based on field name or other criteria
+        return "{}"; // Placeholder, should be replaced with actual metadata generation logic
     }
 
     protected function generateImports($modelName, $relatedModels)
@@ -105,8 +171,7 @@ EOT;
             return "import $relatedModel from './$relatedModel';";
         }, $relatedModels);
 
-        array_unshift($imports, "import { Model } from '@vuex-orm/core';");
+        array_unshift($imports, "import MyBaseModel from '@/models/MyBaseModel';");
         return implode("\n", $imports);
     }
 }
-?>
